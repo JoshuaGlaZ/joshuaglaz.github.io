9439a79c41b03dcaa88da656c6f5716c1c490423
JoshuaGlaZ <128287164+JoshuaGlaZ@users.noreply.github.com>
2025-07-22 20:13:08 +0700

    update deploy.yml with pnpm

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/joshuaglaz.github.io",
+  assetPrefix: "/joshuaglaz.github.io/",
+  trailingSlash: true,
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 	experimental: {
 		mdxRs: true,
